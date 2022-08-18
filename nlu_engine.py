55935e5e8e065efc3ebed059fb11bd937d3ebbd5
Taeho Kim <tk3718@ic.ac.uk>
2022-08-18 22:02:10 +0900

    Potential bug fix

--- nlu_engine.py
+++ nlu_engine.py
@@ -1,10 +1,10 @@
 import io
 import json
 
-from snips_nlu import SnipsNLUEngine
+from snips_nlu import SnipsNLUEngine, load_resources
 from snips_nlu.default_configs import CONFIG_EN
 
-nlu_engine = SnipsNLUEngine(config=CONFIG_EN)
+nlu_engine = SnipsNLUEngine(resources=load_resources("snips_nlu_en"))
 
 with io.open("dataset.json", 'r') as f:
     dataset = json.load(f)
