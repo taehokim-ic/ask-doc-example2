e8c57c286d70cd968f52f9dc22077d4d3821956b
Taeho Kim <tk3718@ic.ac.uk>
2022-08-26 20:03:30 +0900

    Seed added

--- nlu_engine.py
+++ nlu_engine.py
@@ -3,7 +3,8 @@ import json
 
 from snips_nlu import SnipsNLUEngine, load_resources
 
-nlu_engine = SnipsNLUEngine(resources=load_resources("snips_nlu_en"))
+seed = 32
+nlu_engine = SnipsNLUEngine(resources=load_resources("snips_nlu_en"), random_state=seed)
 
 with io.open("dataset2.json", 'r') as f:
     dataset = json.load(f)
