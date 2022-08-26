b6e3c27cd0c71566eccda08a26391ceef382d7af
Taeho Kim <tk3718@ic.ac.uk>
2022-08-27 00:33:51 +0900

    Ipdated ENgine

--- nlu_engine.py
+++ nlu_engine.py
@@ -6,7 +6,7 @@ from snips_nlu import SnipsNLUEngine, load_resources
 seed = 32
 nlu_engine = SnipsNLUEngine(resources=load_resources("snips_nlu_en"), random_state=seed)
 
-with io.open("dataset2.json", 'r') as f:
+with io.open("dataset.json", 'r') as f:
     dataset = json.load(f)
 
 nlu_engine.fit(dataset)
